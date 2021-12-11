fc1c337aa2f4e2ca195dd4ed6972387738e1906b
Ejiro Andrew Okogu <ejirookogu@gmail.com>
2021-12-11 14:59:21 +0100

    new commit

--- account/models.py
+++ account/models.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 import uuid
+
+from django.db.models.fields import BigIntegerField
 # Create your models here.
 
 class CustomUser(AbstractUser):
 # class User(AbstractUser):    
-    id = models.UUIDField(primary_key=True, unique=True, editable=False, default=uuid.uuid4)
+    # id = models.UUIDField(primary_key=True, unique=True, editable=False, default=uuid.uuid4)
+    id = models.BigIntegerField(primary_key=True, unique=True, editable=False, default=uuid.uuid4)
     # address = models.TextField(null=True, blank=True)
     # email = models.EmailField(max_length=30, null=True, blank=True)
 
