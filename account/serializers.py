918d55cf2b59a99e6599a4d0a5dfc02a445cefdf
Ejiro Andrew Okogu <ejirookogu@gmail.com>
2021-12-14 09:58:03 +0100

    new update

--- account/serializers.py
+++ account/serializers.py
@@ -9,7 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         # fields = '__all__'
-        fields = ['first_name','last_name','username','password','email','is_active','date_joined']
+        fields = ['id','first_name','last_name','username','password','email','is_active','date_joined']
         
         
 class ChangePasswordSerializer(serializers.Serializer):
